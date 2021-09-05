ec5a63f932c61189d7df55ad6e4b3571b800288c
nonjosh <nonjosh@gmail.com>
2021-09-05 09:11:18 +0000

    Update char page, support all 4 chars

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app dark>
     <v-navigation-drawer
-      v-model="drawer"
       :mini-variant="miniVariant"
-      :clipped="clipped"
       fixed
       app
+      expand-on-hover
+      permanent
     >
       <v-list>
         <v-list-item
@@ -24,39 +24,6 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
@@ -69,9 +36,6 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
       items: [
         {
           icon: 'mdi-apps',
@@ -79,9 +43,9 @@ export default {
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Items',
-          to: '/items'
+          icon: 'mdi-account-group',
+          title: 'Chars',
+          to: '/chars'
         }
       ],
       miniVariant: false,
--- pages/chars.vue
+++ pages/chars.vue
@@ -2,17 +2,22 @@
   <div class="char-panel">
     <v-toolbar>
       <v-tabs>
-        <v-tab>{{ char_name }}</v-tab>
+        <v-tab
+          v-for="char in chars"
+          :key="char.id"
+          @click="currentCharId = char.id"
+          >{{ char.char_name }}</v-tab
+        >
       </v-tabs>
     </v-toolbar>
     <div class="detail-list">
-      <v-tabs v-model="tab">
+      <v-tabs v-model="currentDetailTab">
         <!-- <v-tab> Stat </v-tab> -->
         <v-tab> Feats </v-tab>
         <v-tab> Powers </v-tab>
         <v-tab> Items </v-tab>
       </v-tabs>
-      <v-tabs-items v-model="tab">
+      <v-tabs-items v-model="currentDetailTab">
         <v-tab-item v-for="tabOption in tabOptions" :key="tabOption.tab">
           <!-- <div v-if="item.tab == 'Stat'">
             <div v-for="stat in charStat" :key="stat.name">
@@ -20,23 +25,34 @@
             </div>
           </div> -->
           <div v-if="tabOption.tab == 'Feats'">
-            <div v-for="feat in char_feats" :key="feat.name">
+            <div
+              v-for="feat in chars[currentCharId].char_feats"
+              :key="feat.name"
+            >
               <v-btn small block :href="feat.url" target="result">{{
                 feat.name
               }}</v-btn>
             </div>
           </div>
           <div v-if="tabOption.tab == 'Powers'">
-            <div v-for="power in charPowers" :key="power.name">
+            <div
+              v-for="power in chars[currentCharId].charPowers"
+              :key="power.name"
+            >
               <v-btn small block :href="power.url" target="result"
-                >{{ power.usage }} {{ power.name }}</v-btn
+                ><v-badge inline left :content="power.usage">
+                  {{ power.name }}</v-badge
+                ></v-btn
               >
             </div>
           </div>
           <div v-if="tabOption.tab == 'Items'">
             <h4>Equipped</h4>
             <v-divider></v-divider>
-            <div v-for="(item, index) in charItems" :key="'E' + index">
+            <div
+              v-for="(item, index) in chars[currentCharId].charItems"
+              :key="'E' + index"
+            >
               <div v-if="item.equip == 1">
                 <v-btn small block :href="item.url" target="result">{{
                   item.name
@@ -46,7 +62,10 @@
             <v-divider></v-divider>
             <h4>Consumables</h4>
             <v-divider></v-divider>
-            <div v-for="(item, index) in charItems" :key="'C' + index">
+            <div
+              v-for="(item, index) in chars[currentCharId].charItems"
+              :key="'C' + index"
+            >
               <div v-if="item.count > 1">
                 <v-btn small block :href="item.url" target="result">
                   <v-badge inline :content="item.count"
@@ -58,7 +77,10 @@
             <v-divider></v-divider>
             <h4>Others</h4>
             <v-divider></v-divider>
-            <div v-for="(item, index) in charItems" :key="'O' + index">
+            <div
+              v-for="(item, index) in chars[currentCharId].charItems"
+              :key="'O' + index"
+            >
               <div v-if="item.count == 1 && item.equip == 0">
                 <v-btn small block :href="item.url" target="result">
                   {{ item.name }}</v-btn
@@ -79,19 +101,69 @@
 import xml2js from 'xml2js'
 import axios from 'axios'
 
+function sortPower(a, b) {
+  if (a.name < b.name) {
+    return -1
+  }
+  if (a.name > b.name) {
+    return 1
+  }
+  return 0
+}
+function sortByObjName(a, b) {
+  if (a.name < b.name) {
+    return -1
+  }
+  if (a.name > b.name) {
+    return 1
+  }
+  return 0
+}
 export default {
   name: 'CharPanel',
   data() {
     return {
       title: 'Chars',
-      file_data: '',
-      char_url: 'https://nonjosh.com/downloads/char/nonjoshiii.dnd4e',
-      char_name: '',
-      charStat: [],
-      char_feats: [],
-      charPowers: [],
-      charItems: [],
-      tab: null,
+      currentCharId: 0,
+      chars: [
+        {
+          id: 0,
+          char_url: 'https://nonjosh.com/downloads/char/nonjoshiii.dnd4e',
+          char_name: '',
+          charStat: [],
+          char_feats: [],
+          charPowers: [],
+          charItems: [],
+        },
+        {
+          id: 1,
+          char_url: 'https://nonjosh.com/downloads/char/jonahbreather.dnd4e',
+          char_name: '',
+          charStat: [],
+          char_feats: [],
+          charPowers: [],
+          charItems: [],
+        },
+        {
+          id: 2,
+          char_url: 'https://nonjosh.com/downloads/char/michaelcrusader.dnd4e',
+          char_name: '',
+          charStat: [],
+          char_feats: [],
+          charPowers: [],
+          charItems: [],
+        },
+        {
+          id: 3,
+          char_url: 'https://nonjosh.com/downloads/char/ken_sharti (1).dnd4e',
+          char_name: '',
+          charStat: [],
+          char_feats: [],
+          charPowers: [],
+          charItems: [],
+        },
+      ],
+      currentDetailTab: null,
       tabOptions: [
         // { tab: "Stat", content: "Stat Content" },
         { tab: 'Feats', content: 'Feats Content' },
@@ -105,171 +177,170 @@ export default {
       title: this.title,
     }
   },
+  computed: {
+    currentChar() {
+      return this.chars[this.currentCharId]
+    },
+  },
   mounted() {
-    axios
-      .get(this.char_url)
-      .then((response) => {
-        const parser = new xml2js.Parser()
-        this.file_data = response.data
+    this.chars.forEach((char) => {
+      axios
+        .get(char.char_url)
+        .then((response) => {
+          const parser = new xml2js.Parser()
 
-        let charJson
-        parser.parseString(response.data, (err, rst) => {
-          if (err) {
-            console.log(err.stack)
-          }
-          charJson = rst
-        })
+          let charJson
+          parser.parseString(response.data, (err, rst) => {
+            if (err) {
+              console.log(err.stack)
+            }
+            charJson = rst
+          })
 
-        // render char name
-        this.char_name =
-          charJson.D20Character.CharacterSheet[0].Details[0].name[0]
+          // render char name
+          char.char_name =
+            charJson.D20Character.CharacterSheet[0].Details[0].name[0]
 
-        // render char stat
-        const charStat =
-          charJson.D20Character.CharacterSheet[0].StatBlock[0].Stat
-        charStat.forEach((statObj) => {
-          const statObjName = statObj.alias[0].$.name
-          const statObjValue = statObj.$.value
-          if (
-            statObjName.endsWith('Defense') ||
-            statObjName.endsWith('AC') ||
-            statObjName.endsWith('Initiative') ||
-            statObjName.endsWith('Hit Points')
-          ) {
-            const stat = {}
-            stat.name = statObjName
-            stat.value = statObjValue
-            this.charStat.push(stat)
-          }
-        })
+          // render char stat
+          const charStat =
+            charJson.D20Character.CharacterSheet[0].StatBlock[0].Stat
+          charStat.forEach((statObj) => {
+            const statObjName = statObj.alias[0].$.name
+            const statObjValue = statObj.$.value
+            if (
+              statObjName.endsWith('Defense') ||
+              statObjName.endsWith('AC') ||
+              statObjName.endsWith('Initiative') ||
+              statObjName.endsWith('Hit Points')
+            ) {
+              const stat = {}
+              stat.name = statObjName
+              stat.value = statObjValue
+              char.charStat.push(stat)
+            }
+          })
 
-        // feat list\
-        const RulesElementTally =
-          charJson.D20Character.CharacterSheet[0].RulesElementTally[0]
-            .RulesElement
-        RulesElementTally.forEach((RulesElement) => {
-          const elementType = RulesElement.$.type
-          if (elementType === 'Feat') {
-            const feat = {}
-            feat.name = RulesElement.$.name
-            feat.url = feat.name.replace(
-              / \(Strength\)| \(Constitution\)| \(Dexterity\)| \(Intelligence\)| \(Wisdom\)| \(Charisma\)/,
+          // feat list\
+          const RulesElementTally =
+            charJson.D20Character.CharacterSheet[0].RulesElementTally[0]
+              .RulesElement
+          RulesElementTally.forEach((RulesElement) => {
+            const elementType = RulesElement.$.type
+            if (elementType === 'Feat') {
+              const feat = {}
+              feat.name = RulesElement.$.name
+              feat.url = feat.name.replace(
+                / \(Strength\)| \(Constitution\)| \(Dexterity\)| \(Intelligence\)| \(Wisdom\)| \(Charisma\)/,
+                ''
+              )
+              feat.url = feat.url.replaceAll(' ', '-').replaceAll("'", '')
+              feat.url =
+                'https://data.dnd.nonjosh.com/compendium/feat/' +
+                feat.url +
+                '.html'
+              char.char_feats.push(feat)
+            }
+          })
+
+          // render power list
+          const charPowers =
+            charJson.D20Character.CharacterSheet[0].PowerStats[0].Power
+          charPowers.forEach((powerObj) => {
+            const power = {}
+            power.name = powerObj.$.name
+            power.usage = powerObj.specific[0]._
+            power.action = powerObj.specific[1]._
+            switch (power.name) {
+              case 'Melee Basic Attack':
+                power.url =
+                  'https://data.dnd.nonjosh.com/compendium/glossary/Basic-Attack.html'
+                break
+              case 'Ranged Basic Attack':
+                power.url =
+                  'https://data.dnd.nonjosh.com/compendium/glossary/Basic-Attack.html'
+                break
+              case 'Bull Rush Attack':
+                power.url =
+                  'https://data.dnd.nonjosh.com/compendium/glossary/Bull-Rush.html'
+                break
+              case 'Grab Attack':
+                power.url =
+                  'https://data.dnd.nonjosh.com/compendium/glossary/Grab.html'
+                break
+              case 'Opportunity Attack':
+                power.url =
+                  'https://data.dnd.nonjosh.com/compendium/glossary/Opportunity-Attack.html'
+                break
+              case 'Second Wind':
+                power.url =
+                  'https://data.dnd.nonjosh.com/compendium/glossary/Second-wind.html'
+                break
+              case 'Refire the Forge Attack':
+                power.url =
+                  'https://data.dnd.nonjosh.com/compendium/power/Refire-the-Forge.html'
+                break
+              default:
+                power.url =
+                  'https://data.dnd.nonjosh.com/compendium/power/' +
+                  power.name.replaceAll(' ', '-').replaceAll("'", '') +
+                  '.html'
+            }
+            char.charPowers.push(power)
+          })
+          // sort item list
+          char.charPowers.sort(sortPower)
+
+          // render item list
+          const LootTally =
+            charJson.D20Character.CharacterSheet[0].LootTally[0].loot
+          LootTally.forEach((lootObj) => {
+            // console.log(lootObj);
+            const item = {}
+            const RulesElement =
+              lootObj.RulesElement[lootObj.RulesElement.length - 1]
+            item.name = RulesElement.$.name
+
+            const advGear = [
+              "Adventurer's Kit",
+              "Climber's Kit",
+              "Thieves' Tools",
+              'Tent',
+              'Fire Kit',
+              'Torch',
+              'Common Meal',
+              'Alchemical Reagents (Arcana)',
+            ]
+            if (advGear.includes(item.name)) {
+              return
+            }
+
+            item.count = parseInt(lootObj.$.count)
+
+            item.equip = lootObj.$['equip-count'] === '1'
+            let url = item.name.replace(
+              / \(heroic tier\)| \(paragon tier\)| \(epic tier\)/,
               ''
             )
-            feat.url = feat.url.replaceAll(' ', '-').replaceAll("'", '')
-            feat.url =
-              'https://data.dnd.nonjosh.com/compendium/feat/' +
-              feat.url +
-              '.html'
-            this.char_feats.push(feat)
-          }
+            if (url.lastIndexOf(' (level') > -1) {
+              url = url.substring(0, url.lastIndexOf(' (level'))
+            }
+            url = url.replace(/ \+1| \+2| \+3| \+4| \+5| \+6|/g, '')
+            url = url.replaceAll(' ', '-').replaceAll("'", '')
+            url = url.replaceAll(/\(|\)/g, '')
+            item.url =
+              'https://data.dnd.nonjosh.com/compendium/item/' + url + '.html'
+
+            char.charItems.push(item)
+          })
+          // sort item list
+          char.charItems.sort(sortByObjName)
         })
-
-        // render power list
-        const charPowers =
-          charJson.D20Character.CharacterSheet[0].PowerStats[0].Power
-        charPowers.forEach((powerObj) => {
-          const power = {}
-          power.name = powerObj.$.name
-          power.usage = powerObj.specific[0]._
-          power.action = powerObj.specific[1]._
-          switch (power.name) {
-            case 'Melee Basic Attack':
-              power.url =
-                'https://data.dnd.nonjosh.com/compendium/glossary/Basic-Attack.html'
-              break
-            case 'Ranged Basic Attack':
-              power.url =
-                'https://data.dnd.nonjosh.com/compendium/glossary/Basic-Attack.html'
-              break
-            case 'Bull Rush Attack':
-              power.url =
-                'https://data.dnd.nonjosh.com/compendium/glossary/Bull-Rush.html'
-              break
-            case 'Grab Attack':
-              power.url =
-                'https://data.dnd.nonjosh.com/compendium/glossary/Grab.html'
-              break
-            case 'Opportunity Attack':
-              power.url =
-                'https://data.dnd.nonjosh.com/compendium/glossary/Opportunity-Attack.html'
-              break
-            case 'Second Wind':
-              power.url =
-                'https://data.dnd.nonjosh.com/compendium/glossary/Second-wind.html'
-              break
-            case 'Refire the Forge Attack':
-              power.url =
-                'https://data.dnd.nonjosh.com/compendium/power/Refire-the-Forge.html'
-              break
-            default:
-              power.url =
-                'https://data.dnd.nonjosh.com/compendium/power/' +
-                power.name.replaceAll(' ', '-').replaceAll("'", '') +
-                '.html'
-          }
-          this.charPowers.push(power)
+        .catch((err) => {
+          // Manage the state of the application if the request
+          // has failed
+          console.log(err)
         })
-
-        // render item list
-        const LootTally =
-          charJson.D20Character.CharacterSheet[0].LootTally[0].loot
-        LootTally.forEach((lootObj) => {
-          // console.log(lootObj);
-          const item = {}
-          const RulesElement =
-            lootObj.RulesElement[lootObj.RulesElement.length - 1]
-          item.name = RulesElement.$.name
-
-          const advGear = [
-            "Adventurer's Kit",
-            "Climber's Kit",
-            "Thieves' Tools",
-            'Tent',
-            'Fire Kit',
-            'Torch',
-            'Common Meal',
-            'Alchemical Reagents (Arcana)',
-          ]
-          if (advGear.includes(item.name)) {
-            return
-          }
-
-          item.count = parseInt(lootObj.$.count)
-
-          item.equip = lootObj.$['equip-count'] === '1'
-          let url = item.name.replace(
-            / \(heroic tier\)| \(paragon tier\)| \(epic tier\)/,
-            ''
-          )
-          if (url.lastIndexOf(' (level') > -1) {
-            url = url.substring(0, url.lastIndexOf(' (level'))
-          }
-          url = url.replace(/ \+1| \+2| \+3| \+4| \+5| \+6|/g, '')
-          url = url.replaceAll(' ', '-').replaceAll("'", '')
-          url = url.replaceAll(/\(|\)/g, '')
-          item.url =
-            'https://data.dnd.nonjosh.com/compendium/item/' + url + '.html'
-
-          this.charItems.push(item)
-        })
-        // sort item list
-        function compare(a, b) {
-          if (a.name < b.name) {
-            return -1
-          }
-          if (a.name > b.name) {
-            return 1
-          }
-          return 0
-        }
-        this.charItems.sort(compare)
-      })
-      .catch((err) => {
-        // Manage the state of the application if the request
-        // has failed
-        console.log(err)
-      })
+    })
   },
 }
 </script>
